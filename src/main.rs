eb9f3c42ef85bd3572c076ed40e0598043db7d34
Eugene Theriault <hybridbassist@gmail.com>
2021-05-21 21:47:48 -0700

    Refactor filename and query variables into Struct

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,30 @@ fn main() {
 
     // index 0 is the file from which this is run (courtesy of std::env::args)
     // will panic if no args given from cli
-    let query = &args[1]; // reference
-    let filename = &args[2];
+    let config = parse_config(&args);
 
-    println!("You're searching for {}", query);
-    println!("In {}", filename);
+    println!("You're searching for {}", config.query);
+    println!("In {}", config.filename);
 
     // read in file content to a string
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong with reading in the file");
 
     println!("With text:\n\n{}", contents)
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+// takes collection of strings as args; returns Config struct
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone(); // clones values for storage in config struct
+    let filename = args[2].clone(); // less efficient, but easier to do
+
+    Config {
+        query,
+        filename,
+    }
 }
